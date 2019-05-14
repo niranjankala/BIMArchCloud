07344823042cf5e06c772f9738974517ebc1fcec
Niranjan Kala <niranjan.kala@gmail.com>
2019-05-14 07:40:50 +0530

    Revert "routing and component additon"
    This reverts commit bbc0d92161db3459570c4f9521bbbb3d7cea1624.

--- src/app/pages/babylon/loaders/loaders.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'bac-babylon-loaders',
-  styleUrls: ['./loaders.component.scss'],
-  templateUrl: './loaders.component.html',
-})
-export class BabylonLoadersComponent {
-}
--- src/app/pages/threejs/loaders/objloader/objloader.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'bac-threejs-objloader',
-  styleUrls: ['./objloader.component.scss'],
-  templateUrl: './objloader.component.html',
-})
-export class ThreeJsObjLoaderComponent {
-}
